15f2043983aeeb02b814b45e9f1ead126bdb737d
Raman S Aujla <ramanpreet.aujla@gmail.com>
2026-04-02 19:01:24 -0400

    chore: GRA-22 - add static MVP routes and shell pages (#18)
    Co-authored-by: Ramanpreet Aujla <ramanpreetaujla@Ramanpreets-Mac-mini.local>

---
+++ apps/ui/src/components/pages/contact-page.tsx
@@ -0,0 +1,7 @@
+import { contactPageContent } from "../../content/pages";
+
+import { StaticContentPage } from "./static-content-page";
+
+export function ContactPage() {
+  return <StaticContentPage content={contactPageContent} />;
+}
